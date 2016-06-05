4a16d50eac70e19d83aca54e7936bd2afc289211
Tony Golding <tpg@blueyonder.co.uk>
2016-06-05 21:24:43 +0100

    Calculate total cost of just apples

--- src/test/scala/valtech/CheckoutCalculatorSpec.scala
+++ src/test/scala/valtech/CheckoutCalculatorSpec.scala
@@ -12,12 +12,35 @@ class CheckoutCalculatorSpec extends FeatureSpec with GivenWhenThen with MustMat
       And("an apple costs 60p and an orange cost 25p")
       val items = Seq("Apple", "Apple", "Orange", "Apple")
 
-      When(s"the checkout calculator calculates the cost of the $items")
+      When(s"the checkout calculator calculates the cost of the ${items.mkString(", ")}")
       val totalCost = new CheckoutCalculator().calculateTotalCost(items)
 
       Then("the total cost is £2.05")
       totalCost must be("£2.05")
     }
-  }
 
+    scenario("Calculate the cost of a collection of just apples") {
+      Given("a collection of two apples")
+      And("an apple costs 60p and an orange cost 25p")
+      val items = Seq("Apple", "Apple")
+
+      When(s"the checkout calculator calculates the cost of the ${items.mkString(", ")}")
+      val totalCost = new CheckoutCalculator().calculateTotalCost(items)
+
+      Then("the total cost is £1.20")
+      totalCost must be("£1.20")
+    }
+
+    scenario("Calculate the cost of a collection of just apples") {
+      Given("a collection of two apples")
+      And("an apple costs 60p and an orange cost 25p")
+      val items = Seq("Apple", "Apple")
+
+      When(s"the checkout calculator calculates the cost of the ${items.mkString(", ")}")
+      val totalCost = new CheckoutCalculator().calculateTotalCost(items)
+
+      Then("the total cost is £1.20")
+      totalCost must be("£1.20")
+    }
+  }
 }
