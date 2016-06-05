d2f029edd4bfd0a3c0cfef101bc162bf0ed90245
Tony Golding <tpg@blueyonder.co.uk>
2016-06-05 21:21:11 +0100

    Calculate the total cost

---
+++ src/test/scala/valtech/CheckoutCalculatorSpec.scala
@@ -0,0 +1,23 @@
+package valtech
+
+import org.scalatest.{MustMatchers, GivenWhenThen, FeatureSpec}
+
+class CheckoutCalculatorSpec extends FeatureSpec with GivenWhenThen with MustMatchers {
+  info("Shopping cart")
+  info("A shop only sells apples and oranges")
+
+  feature("Step one") {
+    scenario("Calculate the cost of a collection of apples and oranges") {
+      Given("a collection of three apples and one orange")
+      And("an apple costs 60p and an orange cost 25p")
+      val items = Seq("Apple", "Apple", "Orange", "Apple")
+
+      When(s"the checkout calculator calculates the cost of the $items")
+      val totalCost = new CheckoutCalculator().calculateTotalCost(items)
+
+      Then("the total cost is £2.05")
+      totalCost must be("£2.05")
+    }
+  }
+
+}
